52b9ef3c38bb5c873abb04d12a5736f2426bfccd
OkaRitsu <riku26clm@gmail.com>
2025-02-02 18:51:59 +0900

    update: train

--- hover_train.py
+++ hover_train.py
@@ -47,7 +47,7 @@ def get_train_cfg(exp_name, max_iterations):
             "resume_path": None,
             "run_name": "",
             "runner_class_name": "runner_class_name",
-            "save_interval": 100,
+            "save_interval": 1000,
         },
         "runner_class_name": "OnPolicyRunner",
         "seed": 1,
@@ -135,6 +135,7 @@ def main():
         obs_cfg=obs_cfg,
         reward_cfg=reward_cfg,
         command_cfg=command_cfg,
+        show_viewer=args.vis,
         device=device,
     )
 
